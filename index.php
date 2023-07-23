9752140e73d58288a5368c99cfc3fb9c3f40045c
Sarvinder kumar <125903255+Sarvinderkumar@users.noreply.github.com>
2023-07-23 08:55:37 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+
+
+<!-----------Head------------>
+	<?php include('include/head.php'); ?>
+<!-----------Head end------------>
+
+
+<!-----------Navigation------------>
+	<?php include('include/navigation.php'); ?>
+<!-----------Navigation end------------>
+
+
+<!-----------Slider------------>
+	<?php include('include/slider.php'); ?>
+<!-----------Slider end------------>
+
+
+<!-----------Carousel------------>
+	<?php include('include/carousel.php'); ?>
+<!-----------Carousel end------------>
+
+
+<!-----------upcoming------------>
+	<? include('include/upcoming.php'); ?>
+<!-----------upcoming end------------>
+
+
+<!-----------Card------------>
+	<?php include('include/cards.php'); ?>
+<!-----------Card end------------>
+
+
+<!-----------Carousel------------>
+	<?php include('include/brands.php'); ?>
+<!-----------Carousel end------------>
+
+
+<!-----------Carousel------------>
+	<?php include('include/map&form.php'); ?>
+<!-----------Carousel end------------>
+
+
+<!-----------Footer------------>
+	<?php include('include/app.php'); ?>
+<!-----------Footer end------------>
+
+
+<!-----------Footer------------>
+	<?php include('include/footer.php'); ?>
+<!-----------Footer end------------>
+
